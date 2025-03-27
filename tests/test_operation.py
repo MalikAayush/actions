e2f8881fbcb7bdb2a7416b5ff74bd9a886be99d1
MalikAayush <aayush.malik@gi-de.com>
2025-03-27 13:58:36 +0530

    name chnaged

---
+++ tests/test_operation.py
@@ -0,0 +1,11 @@
+from src.opertaion import add,sub
+
+def test_add():
+    assert add(2,3)==5
+    assert add(5,6)==11
+    assert add(4,5)==9
+
+def test_sub():
+    assert sub(5,2)==3
+    assert sub(5,6)==-1
+    assert sub(3,3)==0
